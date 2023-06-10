cacf33cf5d070cba9bcc6061d9f7a0bfb235a241
Liana Virabyan <lanvirabyan@gmail.com>
2023-06-10 10:55:08 +0400

    CSS and HTML file

---
+++ Liana Virabyan_frontend_45.js
@@ -0,0 +1,19 @@
+let dateTable =[
+  
+  {'lesson':'Lesson',
+  'name':'Name',
+  'info':"Info"
+  },
+  {'lesson':'js',
+  'name':'Jon',
+  'info':"Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP."
+  },
+  {'lesson':'C++',
+  'name':'Ann',
+  'info':" starting from scratch and go on to advanced concepts like OOP."
+  },
+  {'lesson':'js',
+  'name':'Jon',
+  'info':"Here P."
+  }
+];
